0ba485f41481aaceb04e7c156c397678b3b5bdee
WiseyXD <aryan.s.nag@gmail.com>
2024-02-15 17:30:44 +0530

    build : Wroking on UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
+import { Button } from "@/components/ui/button";
+import AchievementForm from "./components/shared/AchievementForm";
+
 function App() {
     // Add Routing through tanstack router
     const name = "Aryan";
 
-    return <>{name}</>;
+    return (
+        <>
+            <AchievementForm />
+        </>
+    );
 }
 
 export default App;
