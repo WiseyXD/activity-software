6b69570a1dffdfd80e981e6b30395cb4d1e3d1cb
WiseyXD <aryan.s.nag@gmail.com>
2024-02-17 10:32:20 +0530

    feat : Logout Feature Added

--- client/src/components/shared/Navbar.tsx
+++ client/src/components/shared/Navbar.tsx
@@ -1,23 +1,39 @@
 import { RootState } from "@/app/store";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ModeToggle } from "../mode-toggle";
 import { Button } from "../ui/button";
+import { unsetUser } from "@/features/auth/authSlice";
 
 export default function Navbar() {
     const { token: isAuthorized, isAdmin } = useSelector(
         (state: RootState) => state.root.auth
     );
+    const dispatch = useDispatch();
+    function handleLogout() {
+        dispatch(unsetUser());
+    }
 
     return isAuthorized ? (
         isAdmin ? (
-            <p>Admin Here</p>
+            <div className="flex justify-between items-center px-4 py-2 transition-opacity bg-opacity-75">
+                <h1 className="text-2xl">Actify</h1>
+                <div className="flex gap-3">
+                    <ModeToggle />
+                    <Button onClick={handleLogout}>Logout</Button>
+                </div>
+            </div>
         ) : (
-            <p>User here</p>
+            <div className="flex justify-between items-center px-4 py-2 transition-opacity bg-opacity-75">
+                <h1 className="text-2xl">Actify</h1>
+                <div className="flex gap-3">
+                    <ModeToggle />
+                    <Button onClick={handleLogout}>Logout</Button>
+                </div>
+            </div>
         )
     ) : (
         <div className="flex justify-between items-center px-4 py-2 transition-opacity bg-opacity-75">
             <h1 className="text-2xl">Actify</h1>
-            <ModeToggle />
         </div>
     );
 }
